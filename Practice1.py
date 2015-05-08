46244b8da72cd8e331321df0eeb3beb75a8ab10e
isapalau <16ipalau@iaa.edu.ec>
2015-05-08 08:28:24 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,13 @@
+Name = "Isabella Palau"
+Age = 17
+PhoneNumber = 55555555
+Address = "1st Street, 2nd avenue"
+Email = "email@email.com"
+
+print Name
+print Age
+print PhoneNumber
+print Address
+print Email
+
+print "My name is ", Name, ". I am"age"years old. My Number is" PhoneNumber ". I live in "Address". My email is " email "."
