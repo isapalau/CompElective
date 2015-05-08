9142e1fe81b751ca28d78e5073577367e54c1c12
isapalau <16ipalau@iaa.edu.ec>
2015-05-08 08:30:47 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,11 @@
+operand1= 95
+operand2= 64.95
+print "95 + 64.5 =", operand1 + operand2
+print "95 - 64.5 =", operand1 - operand2
+print "95 * 64.5 =", operand1 * operand2
+print "95 / 64.5 =", operand1 / operand2
+print "95 % 64.5 =", operand1 % operand2
+
+# My solution for step 16 is best because it's accuret
+
+
